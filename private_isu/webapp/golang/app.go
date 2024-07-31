69e67a2a5b926ba83e65f7798e5b46cef1f4c8d1
teppei0717 <teppei0717@pepabo.com>
2024-07-31 14:12:27 +0900

    クエリをjsonで取得

--- private_isu/webapp/golang/app.go
+++ private_isu/webapp/golang/app.go
@@ -174,46 +174,45 @@ func getFlash(w http.ResponseWriter, r *http.Request, key string) string {
 func makePosts(results []Post, csrfToken string, allComments bool) ([]Post, error) {
 	var posts []Post
 
+	// 投稿のIDを収集する
+	postIDs := make([]int, len(results))
+	for i, p := range results {
+		postIDs[i] = p.ID
+	}
+
+	// 各投稿のコメントとコメント数を一度に取得する
+	query, args, err := sqlx.In(`
+		SELECT c.*, u.id AS user_id, u.account_name, u.del_flg
+		FROM comments c
+		JOIN users u ON c.user_id = u.id
+		WHERE c.post_id IN (?) AND u.del_flg = 0
+		ORDER BY c.created_at DESC`, postIDs)
+	if err != nil {
+		return nil, err
+	}
+	if !allComments {
+		query += " LIMIT 3"
+	}
+
+	var comments []Comment
+	err = db.Select(&comments, query, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	// コメントをポストにマップする
+	commentMap := make(map[int][]Comment)
+	for _, c := range comments {
+		commentMap[c.PostID] = append(commentMap[c.PostID], c)
+	}
+
+	// 各投稿にコメントを割り当てる
 	for _, p := range results {
-		err := db.Get(&p.CommentCount, "SELECT COUNT(*) AS `count` FROM `comments` WHERE `post_id` = ?", p.ID)
-		if err != nil {
-			return nil, err
-		}
-
-		query := "SELECT * FROM `comments` WHERE `post_id` = ? ORDER BY `created_at` DESC"
-		if !allComments {
-			query += " LIMIT 3"
-		}
-		var comments []Comment
-		err = db.Select(&comments, query, p.ID)
-		if err != nil {
-			return nil, err
-		}
-
-		for i := 0; i < len(comments); i++ {
-			err := db.Get(&comments[i].User, "SELECT * FROM `users` WHERE `id` = ?", comments[i].UserID)
-			if err != nil {
-				return nil, err
-			}
-		}
-
-		// reverse
-		for i, j := 0, len(comments)-1; i < j; i, j = i+1, j-1 {
-			comments[i], comments[j] = comments[j], comments[i]
-		}
-
-		p.Comments = comments
-
-		err = db.Get(&p.User, "SELECT * FROM `users` WHERE `id` = ?", p.UserID)
-		if err != nil {
-			return nil, err
-		}
-
+		p.Comments = commentMap[p.ID]
+		p.CommentCount = len(p.Comments)
 		p.CSRFToken = csrfToken
 
-		if p.User.DelFlg == 0 {
-			posts = append(posts, p)
-		}
+		posts = append(posts, p)
 		if len(posts) >= postsPerPage {
 			break
 		}
